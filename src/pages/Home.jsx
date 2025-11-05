653845b77910b407d0def05986fb81c75f3d5bc0
Nitish <nitishm1803@gmail.com>
2025-11-05 23:04:11 +0530

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+  return <div>
+
+    {/* Theme Toggle */}
+    {/* Background Effects */}
+    {/* NavBar*/}
+    {/* Main Content */}
+    {/* Footer */} 
+  </div>;
+}
