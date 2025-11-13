b56af445ab95c113e3614f8d508a8309f835fffd
Nitish <nitishm1803@gmail.com>
2025-11-13 21:27:41 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,7 +34,7 @@ export const Navbar = () => {
           href="#hero"
         >
           <span className="relative z-10">
-            <span className="text-glow text-foreground"> Nitish's </span>{" "}
+            <span className="text-glow text-foreground"> My </span>{" "}
             Portfolio
           </span>
         </a>
